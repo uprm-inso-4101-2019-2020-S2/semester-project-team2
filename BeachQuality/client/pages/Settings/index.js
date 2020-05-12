b1b2a74d1dfa2304d8022466a46a7396d6e2e3c5
Kevin-Burgos-9 <kevin.burgos4@upr.edu>
2020-05-12 14:37:17 -0400

    Ui for settings done.

--- BeachQuality/client/pages/Settings/index.js
+++ BeachQuality/client/pages/Settings/index.js
@@ -1,91 +1,46 @@
-import React, { useState } from "react";
-import { View, Text, Button, Switch, StyleSheet } from "react-native";
-
-export default function App() {
-
-const [isEnabled, setIsEnabled] = useState(false);
-const [isEnabled1, setIsEnabled1] = useState(false);
-const [isEnabled2, setIsEnabled2] = useState(false);
-const notifSwitch = () => setIsEnabled(previousState => !previousState);
-const darkModeSwitch = () => setIsEnabled1(previousState => !previousState);
-const locationSwitch = () => setIsEnabled2(previousState => !previousState);
-
-  return (
-      <View style = {styles.page}>
-        <View style = {styles.navBar}>
-          <Button 
-          title = "backwards facing arrow" 
-          icon = ""
-          onPress = {() => { navigation.navigate("SOMEONE PLZX MAKE THIS WORK") }}
-          />
-          <Text style={styles.titleText}>Settings</Text>
-        </View>
-        
-        
-        <View style={styles.container}>
-          <View style = {styles.item}>
-            <Text>Notifications:</Text>
-            <Switch
-            trackColor={{ false: "#767577", true: "#81b0ff" }}
-            thumbColor={isEnabled ? "#f4f3f4" : "#f4f3f4"}
-            ios_backgroundColor="#3e3e3e"
-            onValueChange={notifSwitch}
-            value={isEnabled}
-            />
-          </View>
-          <View style = {styles.item}>
-            <Text>Dark Mode:</Text>
-            <Switch
-              trackColor={{ false: "#767577", true: "#81b0ff" }}
-              thumbColor={isEnabled ? "#f4f3f4" : "#f4f3f4"}
-              ios_backgroundColor="#3e3e3e"
-              onValueChange={darkModeSwitch}
-              value={isEnabled1}
+import React, { Component, useState } from 'react';
+import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text, List, ListItem, Switch } from 'native-base';
+export default class AnatomyExample extends Component {
+  render() {
+    return (
+      <Container>
+        <Header>
+          <Left>
+            <Button transparent>
+              <Icon name='ios-arrow-back' />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Settings</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Content>
+        <List>
+            <ListItem>
+              <Body>
+                <Text>Notifications</Text>
+              </Body>
+              <Switch
+              value={true}
+              // onValueChange={toggleSwitch}
               />
-          </View>
-          <View style = {styles.item}>
-            <Text>Loctaion Services:</Text>
-            <Switch
-            trackColor={{ false: "#767577", true: "#81b0ff" }}
-            thumbColor={isEnabled ? "#f4f3f4" : "#f4f3f4"}
-            ios_backgroundColor="#3e3e3e"
-            onValueChange={locationSwitch}
-            value={isEnabled2}
-            />
-          </View>
-       </View>
-      </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  page : {
-    flex: 1,
-  },
-  navBar: {
-    flex : 1,
-    flexDirection: 'row',
-    alignItems: "center",
-    justifyContent: 'space-around',
-    backgroundColor: "#3498DB",
-    paddingTop: 15,
-  },
-
-  titleText: {
-    color: 'white',fontWeight: 'bold',fontSize: 30
-  },
-  container: {
-    flex: 7,
-    paddingHorizontal: 30,
-  },
-  item: {
-    // flex : 3,
-    flexDirection: 'row',
-    alignItems: "center",
-    justifyContent:'space-between',
-    padding: 15,
+            </ListItem>
+            <ListItem>
+              <Body>
+                <Text>Location Services</Text>
+              </Body>
+              <Switch value={true} />
+            </ListItem>
+            <ListItem>
+              <Body>
+                <Text>Dark Mode</Text>
+              </Body>
+              <Switch value={false} />
+            </ListItem>
+          </List>
+        </Content>
+      </Container>
+    );
   }
-});
-
-
-// export default Settings;
+}
