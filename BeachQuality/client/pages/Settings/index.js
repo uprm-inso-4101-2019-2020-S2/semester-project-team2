3ab483403d83c82a3ddab39b81480267c202e969
Luis F. Caro <luisfcaro@pop-os.localdomain>
2020-05-12 14:57:09 -0400

    modifying class components to functional

--- BeachQuality/client/pages/Settings/index.js
+++ BeachQuality/client/pages/Settings/index.js
@@ -1,46 +1,61 @@
-import React, { Component, useState } from 'react';
-import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text, List, ListItem, Switch } from 'native-base';
-export default class AnatomyExample extends Component {
-  render() {
-    return (
-      <Container>
-        <Header>
-          <Left>
-            <Button transparent>
-              <Icon name='ios-arrow-back' />
-            </Button>
-          </Left>
-          <Body>
-            <Title>Settings</Title>
-          </Body>
-          <Right />
-        </Header>
-        <Content>
+import React, { Component, useState } from "react";
+import {
+  Container,
+  Header,
+  Title,
+  Content,
+  Footer,
+  FooterTab,
+  Button,
+  Left,
+  Right,
+  Body,
+  Icon,
+  Text,
+  List,
+  ListItem,
+  Switch
+} from "native-base";
+const Settings = () => {
+  return (
+    <Container>
+      <Header>
+        <Left>
+          <Button transparent>
+            <Icon name="ios-arrow-back" />
+          </Button>
+        </Left>
+        <Body>
+          <Title>Settings</Title>
+        </Body>
+        <Right />
+      </Header>
+      <Content>
         <List>
-            <ListItem>
-              <Body>
-                <Text>Notifications</Text>
-              </Body>
-              <Switch
+          <ListItem>
+            <Body>
+              <Text>Notifications</Text>
+            </Body>
+            <Switch
               value={true}
               // onValueChange={toggleSwitch}
-              />
-            </ListItem>
-            <ListItem>
-              <Body>
-                <Text>Location Services</Text>
-              </Body>
-              <Switch value={true} />
-            </ListItem>
-            <ListItem>
-              <Body>
-                <Text>Dark Mode</Text>
-              </Body>
-              <Switch value={false} />
-            </ListItem>
-          </List>
-        </Content>
-      </Container>
-    );
-  }
-}
+            />
+          </ListItem>
+          <ListItem>
+            <Body>
+              <Text>Location Services</Text>
+            </Body>
+            <Switch value={true} />
+          </ListItem>
+          <ListItem>
+            <Body>
+              <Text>Dark Mode</Text>
+            </Body>
+            <Switch value={false} />
+          </ListItem>
+        </List>
+      </Content>
+    </Container>
+  );
+};
+export default Settings;
