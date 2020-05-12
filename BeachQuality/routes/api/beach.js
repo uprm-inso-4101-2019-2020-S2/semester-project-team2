d9b5adcffc13ef6d551727c2612bf2bba61c5b26
Alejandro Ruiz <alejandro.ruiz2@upr.edu>
2020-05-11 23:28:11 -0400

    Added Fetch Weekly Update

--- BeachQuality/routes/api/beach.js
+++ BeachQuality/routes/api/beach.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const axios = require("axios");
 
 // Beach Model
 const Beach = require("../../models/beach");
@@ -40,5 +41,17 @@ router.post('/addBeach', (req, res) => {
     .then(beach => res.json(beach))
     .catch(err => console.log(err));
 })
+
+// @route   GET api/beach/fetchWeeklyUpdate
+// @desc    Fetch Weekly Update
+// @access  Private
+router.get("/fetchWeeklyUpdate", async(req, res) => {
+  const beachData = [];
+  const fetchData = await axios
+                            .get("https://mmvk4falrj.execute-api.us-west-2.amazonaws.com/v1/history/lab/4")
+                            .then(res => beachData = res.data)
+                            .catch(err => console.log(err));
+                            console.log(beachData);
+});
 
 module.exports = router;
