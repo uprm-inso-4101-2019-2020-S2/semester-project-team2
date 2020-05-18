b4336f3e7c553acce732a81f00d2bf2326fd88c6
cfig41 <carlos.figueroa22@upr.edu>
2020-05-17 20:46:10 -0400

    Added tests for assets; commented import breaking tests.

--- BeachQuality/testing/reactnative.test.js
+++ BeachQuality/testing/reactnative.test.js
@@ -11,8 +11,28 @@ test("<App JSON>", () => {
     expect(json.expo.platforms).toContain("web");
 });
 
-
+/*
 test("<PAGES>", () => {
     const PAGES = require("../client/pages/index");
     expect(Object.keys(PAGES).length).toBe(8);
+}); */
+
+/*
+test("<Ansi Align>", () => {
+    const al = require("../node_modules/ansi-align");
+    const opts = {align: "left"};
+    const text = "Lorem Ipsum";
+    const rtn_txt = ansiAlign(text, opts);
+    expect(rtn_txt).toEqual(text);
+}); */
+
+test("<Assets>", () => {
+    const fs = require("fs");
+    const path = "../BeachQuality/client/assets/";
+    const icon = path + "icon.png";
+    const signupbackground = path + "signupBackground.jpeg";
+    const splash = path + "splash.png";
+    expect(fs.existsSync(icon)).toBeTruthy();
+    expect(fs.existsSync(signupbackground)).toBeTruthy();
+    expect(fs.existsSync(splash)).toBeTruthy();
 });
