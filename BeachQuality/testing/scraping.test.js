2497d888bd18abfe0347bc3b0e18943df804a3b9
Marco2999 <42784695+Marco2999@users.noreply.github.com>
2020-05-18 16:06:33 -0400

    Add files via upload
    Scrape test

---
+++ BeachQuality/testing/scraping.test.js
@@ -0,0 +1,16 @@
+
+const bname = require("../BeachQuality/index.js");
+
+it('Returns the proper beach', () => {
+
+  expect(BeachQuality.index.data.forEach((item, i) => {
+    if(BeachQuality.index.data.indexOf(BeachQuality.index.beachData.name) >= 0 ) {
+      return true;
+    }
+  });
+  );
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+
+})
